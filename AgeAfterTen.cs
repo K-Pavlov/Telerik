61546cad0265d738c852916d6c12d24e12feda66
Kaloqn Pavlov <kaloian.pavlov@abv.bg>
2013-11-10 16:59:48 +0200

    Blop

---
+++ AgeAfterTen.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age;
+        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(age+10);
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(12345*12345);
+    }
+}
+
